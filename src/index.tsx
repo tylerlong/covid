3522df59b8803ae494a87b4e79a22cb055bd8901
Tyler Long <tyler4long@gmail.com>
2022-01-13 21:34:40 -0800

    Hello chart.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Component} from '@tylerlong/use-proxy/build/react';
-import {Spin} from 'antd';
+// import {Spin} from 'antd';
+import Chart, {ChartConfiguration, ChartItem} from 'chart.js/auto';
 
 import './index.css';
 import store, {Store} from './store';
@@ -11,8 +12,33 @@ class App extends Component<{store: Store}> {
     return (
       <>
         <h1>Hello world!</h1>
-        <Spin size="large" />
+        <canvas id="myChart"></canvas>
       </>
+    );
+  }
+  componentDidMount() {
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        },
+      ],
+    };
+
+    const config: ChartConfiguration = {
+      type: 'line',
+      data: data,
+      options: {},
+    };
+    const myChart = new Chart(
+      document.getElementById('myChart') as ChartItem,
+      config
     );
   }
 }
