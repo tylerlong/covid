e5c552b226fe69f41e42a0376b4bd9834437ee12
Tyler Liu <tyler4long@gmail.com>
2022-01-14 20:52:23 -0800

    Add some padding to the page

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Component} from '@tylerlong/use-proxy/build/react';
-import {DatePicker, Divider} from 'antd';
+import {Col, DatePicker, Divider, Row} from 'antd';
 import moment from 'moment';
 
 import './index.css';
@@ -12,30 +12,32 @@ class App extends Component<{store: Store}> {
   render() {
     const {store} = this.props;
     return (
-      <>
-        <h1>COVID-19</h1>
-        <DatePicker.RangePicker
-          disabledDate={d =>
-            !d ||
-            d.isAfter(moment(maxDate, dateFormat)) ||
-            d.isBefore(moment(minDate, dateFormat))
-          }
-          defaultValue={[
-            moment(store.startDate, dateFormat),
-            moment(store.endDate, dateFormat),
-          ]}
-          format={dateFormat}
-          onChange={(dates, dateStrings) => {
-            store.startDate = dateStrings[0];
-            store.endDate = dateStrings[1];
-            store.updateChart();
-          }}
-        />
-        <Divider />
-        <canvas id="confirmedChart"></canvas>
-        <Divider />
-        <canvas id="deathsChart"></canvas>
-      </>
+      <Row>
+        <Col offset={2} span={20}>
+          <h1>COVID-19</h1>
+          <DatePicker.RangePicker
+            disabledDate={d =>
+              !d ||
+              d.isAfter(moment(maxDate, dateFormat)) ||
+              d.isBefore(moment(minDate, dateFormat))
+            }
+            defaultValue={[
+              moment(store.startDate, dateFormat),
+              moment(store.endDate, dateFormat),
+            ]}
+            format={dateFormat}
+            onChange={(dates, dateStrings) => {
+              store.startDate = dateStrings[0];
+              store.endDate = dateStrings[1];
+              store.updateChart();
+            }}
+          />
+          <Divider />
+          <canvas id="confirmedChart"></canvas>
+          <Divider />
+          <canvas id="deathsChart"></canvas>
+        </Col>
+      </Row>
     );
   }
   componentDidMount() {
